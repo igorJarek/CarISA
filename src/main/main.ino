a5d514ecbae9fbc81ed1c0a535e1123e619698c9
Igor <igij@o2.pl>
2019-06-13 20:05:24 +0200

    task 1 ready

--- src/main/main.ino
+++ src/main/main.ino
@@ -9,22 +9,16 @@
 // szybszy kompas tak chociaż 4ks/s
 // przy tym kompasie co mamy wybieramy jakosc pomiaru czy szybkosc działania układu
 
-// problem z usrednieniem w okolicach 0/360 stopni (czesc pomiarow ma wartosc bliska 360 a czesc bliska 0 stopni)
-// rozwiazanie 1 : arcus sinus
-// wady : zakres od -90 do 90 stopni - problemy z odejmowaniem
-// rozwiazanie 2 : wektory
-
-TODO : Sprawdzic i doregulowac obrot o 90 stopni (skreca za daleko np o 110 stopni), przejrzec dane z kompasu
+uint8_t encoderStraightCounter = 20;
 
 void printValue(String s, float value)
 {
-      Serial.print(s + " : ");
-      Serial.println(value, 7);
+      Serial1.print(s + " : ");
+      Serial1.println(value, 7);
 }
 
 void setup() 
 {
-    //Serial.begin(115200);
     Serial1.begin(9600);
 }
 
@@ -32,37 +26,32 @@ void loop()
 {
     uint32_t startEncoder = encoders.getRightCount();
     engines.accelerate(SIDE_BOTH, FORWARD, Engines::MAX_SPEED);
-    while(encoders.getRightCount() <= startEncoder + 25);
+    while(encoders.getRightCount() <= startEncoder + encoderStraightCounter);
     engines.stopImmediately(SIDE_BOTH);
+    delay(500);
     
-    int16_t startX = 0, startY = 0;
-    int16_t currX = 0, currY = 0;
-    float deltaAngle = 0.0f, sumAngle = 0.0f;
-    float maxEpsilon = 0.0f;
-    compas.avgMeasure(200, startX, startY);
-  
-    engines.accelerate(SIDE_LEFT, FORWARD, 90);
-    engines.accelerate(SIDE_RIGHT, BACKWARD, 110);
+    float currAngle = 0.0f, deltaAngle = 0.0f;
+    float startAngle =  compas.avgAngle(1);
+
+    engines.accelerate(SIDE_LEFT, FORWARD, 128);
+    engines.accelerate(SIDE_RIGHT, BACKWARD, 128);
 
     do
     {
-        compas.avgMeasure(5, currX, currY);
-        int32_t iloczyn_skalarny = (startX*currX) + (startY*currY);
-        float aLen = sqrt(pow(startX, 2) + pow(startY, 2));
-        float bLen = sqrt(pow(currX, 2) + pow(currY, 2));
-        float cosinus = iloczyn_skalarny / (aLen * bLen);
-        deltaAngle = acos(cosinus) * (180.0/PI);
-        //deltaAngle = deltaAngle >= 0.5 ? deltaAngle : 0.0f;   
-        sumAngle += deltaAngle;
-        Serial1.print(sumAngle);
-        Serial1.print(" | ");
-        Serial1.println(deltaAngle);
-        
-        startX = currX;
-        startY = currY;
+          currAngle =  compas.avgAngle(1);
+          deltaAngle = fabs(currAngle - startAngle);
+          if(deltaAngle > 180.0f)
+            deltaAngle = 360.0 - deltaAngle;
+
+          Serial1.print(startAngle);
+          Serial1.print(" | ");
+          Serial1.print(currAngle);
+          Serial1.print(" | ");
+          Serial1.println(deltaAngle);
+          delay(100);
     }
-    while(sumAngle < 89.0f);
+    while(deltaAngle < 90.0f);
     
     engines.stopImmediately(SIDE_BOTH);
-    delay(1000);
+    delay(500);
 }
